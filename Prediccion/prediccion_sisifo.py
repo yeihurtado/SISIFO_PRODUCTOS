3b36864b14c9e3a753eeb4de939d8f0bf6e9ecf4
JpenaInteia <130397492+JpenaInteia@users.noreply.github.com>
2024-04-24 15:04:57 -0500

    documentación de funciones

--- Prediccion/prediccion_sisifo.py
+++ Prediccion/prediccion_sisifo.py
@@ -33,7 +33,21 @@ psql_database = os.getenv('database_azure')
 psql_engine = create_engine(f"postgresql://{psql_user}:{psql_password}@{psql_localhost}:{psql_port}/{psql_database}?sslmode=require")
 psql_connection = psql_engine.connect()
 
+"""
+Elimina filas de una tabla en una base de datos PostgreSQL en función de un rango de fechas.
 
+Esta función se conecta a una base de datos PostgreSQL utilizando las credenciales almacenadas en variables de entorno 
+y elimina filas de la tabla especificada donde la columna 'fecha_hora' (fecha-hora) se encuentra dentro del 
+rango de fechas proporcionado (inclusivo).
+
+Parámetros:
+    table_name (str): El nombre de la tabla de la que se eliminarán las filas.
+    date_min (datetime.datetime): La fecha mínima (inclusiva) para el rango de eliminación.
+    date_max (datetime.datetime): La fecha máxima (inclusiva) para el rango de eliminación.
+
+Devuelve:
+    Ninguno
+"""
 def delete_table_month(table_name,date_min,date_max):
     q_ag_fc_dt_dia = f'''          
     DELETE FROM {table_name} WHERE  (               
@@ -47,6 +61,20 @@ def delete_table_month(table_name,date_min,date_max):
     conn.commit()
     conn.close()
 
+"""
+Inserta un DataFrame de Pandas en una tabla PostgreSQL en lotes.
+
+Esta función toma un DataFrame de Pandas (`df`) y el nombre de una tabla PostgreSQL (`table_name`) como entrada.
+Divide el DataFrame en lotes de tamaño especificado y los inserta en la tabla de forma eficiente utilizando la conexión
+a la base de datos establecida mediante SQLAlchemy.
+
+Argumentos:
+    df (pandas.DataFrame): El DataFrame de Pandas que contiene los datos a insertar.
+    table_name (str): El nombre de la tabla PostgreSQL en la que se insertarán los datos.
+
+Devuelve:
+    bool: True si la inserción se realizó correctamente, False si se produjo un error.
+"""
 def df_psql_batch(df,table_name):    
     try:
         batch_size = 5000
@@ -104,24 +132,26 @@ def calculate_df(rail, date_min, date_max,date_max_pred):
     df = pd.merge(df_tmp, df, on='date_hour', how='left')
     
     return df
+
     
-   
+
+"""
+Realiza el cálculo y preprocesamiento de los datos en el DataFrame.
+
+Este método toma el DataFrame almacenado en el objeto y realiza las siguientes operaciones de preprocesamiento:
+1. Elimina filas con valores nulos.
+2. Elimina filas con valores atípicos basados en la puntuación Z.
+3. Almacena el DataFrame preprocesado en el atributo 'df_data' del objeto.
+
+Args:
+    *args: Argumentos posicionales opcionales.
+    **kwargs: Argumentos clave opcionales.
+
+Returns:
+    None
+"""
 def calculate_data(df):
-    """
-    Realiza el cálculo y preprocesamiento de los datos en el DataFrame.
 
-    Este método toma el DataFrame almacenado en el objeto y realiza las siguientes operaciones de preprocesamiento:
-    1. Elimina filas con valores nulos.
-    2. Elimina filas con valores atípicos basados en la puntuación Z.
-    3. Almacena el DataFrame preprocesado en el atributo 'df_data' del objeto.
-
-    Args:
-        *args: Argumentos posicionales opcionales.
-        **kwargs: Argumentos clave opcionales.
-
-    Returns:
-        None
-    """
     
     df=df.copy()
     
@@ -134,27 +164,29 @@ def calculate_data(df):
     # Almacenar el DataFrame con información limpia de nulos y datos atipicos para el entrenamiento del modelo
     return df
 
+
+"""
+Calcula el Modelo de Aprendizaje Profundo (MDL) utilizando una red neuronal secuencial.
+
+Este método construye una red neuronal secuencial con capas densas y lo entrena con los datos
+de características y objetivos proporcionados en el DataFrame df_data. Utiliza la función de pérdida
+de error absoluto medio (MAE) y el optimizador Adam para el entrenamiento.
+
+Args:
+    args: Argumentos posicionales adicionales (no se utilizan en este método).
+    kwargs: Argumentos clave adicionales (no se utilizan en este método).
+
+Returns:
+    None
+
+Nota:
+    Este método modifica los siguientes atributos de la instancia:
+    - score: El valor de la métrica de error absoluto medio (MAE) del modelo en los datos de entrenamiento.
+    - model: El modelo de red neuronal entrenado.
+
+"""
 def calculate_mdl(df_data,target_columns,feature_columns,rail):
-    """
-    Calcula el Modelo de Aprendizaje Profundo (MDL) utilizando una red neuronal secuencial.
 
-    Este método construye una red neuronal secuencial con capas densas y lo entrena con los datos
-    de características y objetivos proporcionados en el DataFrame df_data. Utiliza la función de pérdida
-    de error absoluto medio (MAE) y el optimizador Adam para el entrenamiento.
-
-    Args:
-        args: Argumentos posicionales adicionales (no se utilizan en este método).
-        kwargs: Argumentos clave adicionales (no se utilizan en este método).
-
-    Returns:
-        None
-
-    Nota:
-        Este método modifica los siguientes atributos de la instancia:
-        - score: El valor de la métrica de error absoluto medio (MAE) del modelo en los datos de entrenamiento.
-        - model: El modelo de red neuronal entrenado.
-
-    """
 
     model_file = 'models/' + str(rail) + '.keras'
     
@@ -184,6 +216,43 @@ def calculate_mdl(df_data,target_columns,feature_columns,rail):
 
     return model,score
     
+
+"""
+Realiza predicciones para el DataFrame utilizando un modelo de aprendizaje automático.
+
+Esta función toma un DataFrame preprocesado (`df`), un modelo de aprendizaje automático entrenado (`model`), 
+listas de nombres de columnas para características (`feature_columns`) y predicciones (`predict_columns`), 
+y el identificador del carril (`rail`) como entrada. La función realiza lo siguiente:
+
+1. **Preparación del DataFrame para la predicción:**
+    - Combina el DataFrame original (`df`) con las predicciones realizadas por el modelo.
+    - El modelo se aplica a las columnas especificadas en `feature_columns` del DataFrame y las predicciones se guardan 
+    en un nuevo DataFrame con las columnas especificadas en `predict_columns`.
+    - La función `pd.concat` se utiliza para concatenar el DataFrame original con el DataFrame de predicciones a lo largo 
+    del eje 1 (columnas).
+
+2. **Generación del nombre del archivo CSV (comentado):**
+    - Construye el nombre del archivo CSV basado en el carril (`rail`), fechas mínima y máxima (asumiendo su existencia 
+    en variables `date_min` y `date_max`) y prefijos para diferenciarlo de los archivos sin predicciones.
+    - Esta parte está comentada, lo que indica que la función actualmente no guarda el DataFrame con las predicciones.
+
+3. **Retorno del DataFrame:**
+    - La función devuelve el DataFrame final (`df_final`) que contiene los datos originales combinados con las columnas de predicción.
+
+
+
+**Argumentos:**
+
+* `df` (pandas.DataFrame): El DataFrame preprocesado que contiene los datos para realizar las predicciones.
+* `model` (Machine Learning Model): El modelo de aprendizaje automático entrenado que se utilizará para realizar las predicciones.
+* `feature_columns` (list): Una lista de nombres de columna que representan las características que se usarán para la predicción.
+* `predict_columns` (list): Una lista de nombres de columna para las predicciones realizadas por el modelo.
+* `rail` (str): Identificador del carril del que se procesaron los datos.
+
+**Devuelve:**
+
+* `pandas.DataFrame`: El DataFrame final que contiene los datos originales junto con las columnas de predicción generadas por el modelo.
+"""
 def calculate_predict(df,model, feature_columns,predict_columns,rail):
     df_final=pd.concat([df.reset_index(drop=True), pd.DataFrame(data=model.predict(df[feature_columns]), columns=predict_columns)],axis=1)
 
@@ -193,23 +262,25 @@ def calculate_predict(df,model, feature_columns,predict_columns,rail):
 
     return df_final
     
+
+"""
+Realiza cálculos estadísticos y agrega información a un DataFrame.
+
+Este método calcula diversas estadísticas y agregaciones sobre los datos en un DataFrame
+y almacena los resultados en otro DataFrame llamado 'df_info'. Las estadísticas calculadas
+incluyen valores nulos, valores atípicos y agregaciones de datos en función de distintas
+métricas y atributos.
+
+Args:
+    *args: Argumentos posicionales (no se utilizan en este método).
+    **kwargs: Argumentos clave (no se utilizan en este método).
+    
+Returns:
+    None
+
+"""
 def calculate_info(df_final,model,rail,score):
-    """
-    Realiza cálculos estadísticos y agrega información a un DataFrame.
     
-    Este método calcula diversas estadísticas y agregaciones sobre los datos en un DataFrame
-    y almacena los resultados en otro DataFrame llamado 'df_info'. Las estadísticas calculadas
-    incluyen valores nulos, valores atípicos y agregaciones de datos en función de distintas
-    métricas y atributos.
-    
-    Args:
-        *args: Argumentos posicionales (no se utilizan en este método).
-        **kwargs: Argumentos clave (no se utilizan en este método).
-        
-    Returns:
-        None
-    
-    """
     groups = [
         "rail",
     ]
@@ -256,7 +327,58 @@ def calculate_info(df_final,model,rail,score):
     df_psql_batch(df,"dm_fc_dt_models")
 
 
+"""
+Realiza predicciones de intensidad y velocidad vehicular para carriles específicos en un rango de fechas.
 
+Esta función toma como entrada la fecha mínima (`date_min`), la fecha máxima (`date_max`) y la fecha máxima del rango de predicción (`date_max_pred`). 
+La función realiza los siguientes pasos:
+
+1. **Identificación de carriles sin predicciones:**
+    - Ejecuta una consulta SQL para identificar los carriles que no tienen predicciones existentes en la tabla `fc_dt_pred`.
+    - Almacena los identificadores de los carriles sin predicciones en una lista (`carril_ids`).
+
+2. **Predicción para cada carril:**
+    - Recorre la lista de `carril_ids` para procesar cada carril individualmente.
+    - Para cada carril (`rail`):
+        - Filtra el DataFrame original (`df`) para obtener los datos específicos del carril actual.
+        - Define las columnas de características (`feature_columns`) y las columnas objetivo (`target_columns`) para el modelo.
+        - Define las columnas de predicción (`predict_columns`) que se generarán a través del modelo.
+        - Calcula el DataFrame con los datos del carril y las fechas del rango de predicción (`df_cdf`).
+        - Verifica si existen valores reales de intensidad en el DataFrame (`df_cdf['value_intensity_real'].max() > 0`).
+            - Si existen valores reales de intensidad:
+                - Calcula el conjunto de datos preprocesado (`df_cd`) para el modelo.
+                - Entrena un modelo de aprendizaje automático (`model`) y obtiene su puntuación (`score`).
+                - Realiza predicciones con el modelo entrenado y genera el DataFrame final (`df_final`) con las predicciones.
+                - Imprime la puntuación del modelo (`score`).
+                - **(Comentado)** Guarda el DataFrame final con las predicciones en la tabla `fc_dt_pred` (línea comentada).
+                - Calcula y guarda información adicional sobre el modelo y las predicciones para el carril (`rail`).
+
+3. **Retorno:**
+    - La función no devuelve explícitamente ningún valor.
+    - Guarda el DataFrame final con las predicciones en la tabla `fc_dt_pred` (línea comentada).
+    - Genera información adicional sobre el modelo y las predicciones para cada carril procesado.
+
+**Argumentos:**
+
+* `date_min` (datetime.datetime): Fecha mínima del rango de datos para el análisis.
+* `date_max` (datetime.datetime): Fecha máxima del rango de datos para el análisis.
+* `date_max_pred` (datetime.datetime): Fecha máxima del rango de predicción.
+
+**Devoluciones:**
+
+* No hay devoluciones explícitas.
+* Guarda el DataFrame final con las predicciones en la tabla `fc_dt_pred` (línea comentada).
+* Genera información adicional sobre el modelo y las predicciones para cada carril procesado.
+
+**Consideraciones:**
+
+* La función asume la existencia de tablas `dm_carril` y `fc_dt_pred` en la base de datos PostgreSQL.
+* La función utiliza funciones auxiliares `calculate_df`, `calculate_data`, `calculate_mdl`, `calculate_predict` y `calculate_info` 
+que se presumen definidas en otro lugar del código.
+* La línea que guarda el DataFrame final en la tabla `fc_dt_pred` está actualmente comentada.
+* La función podría mejorarse para manejar casos donde no existen valores reales de intensidad para un carril.
+
+"""
 def prediction_fc_dt(date_min,date_max,date_max_pred):
 
     template_sql = f"""
